f3804db7dcc1029a03b8e75635c5558e146946f3
Kushal <kotikushal05@gmail.com>
2021-03-28 14:45:47 +0530

    Added setter functions for rules, match-details providers

--- lib/Providers/match-details-provider.dart
+++ lib/Providers/match-details-provider.dart
@@ -1,6 +1,22 @@
+import 'package:cric_scorer/Models/match-model.dart';
 import 'package:flutter/material.dart';
 
 class MatchDetailsProvider extends ChangeNotifier {
   // this takes care of all the details regarding a match
   // scores of teams, players
+  Map<String, Rules> rulesObj = new Map();
+  Map<String, Match> matchObj = new Map();
+  String matchId;
+
+  setMatchObj(Match match) {
+    matchId = match.matchId;
+    matchObj[match.matchId] = match;
+    print('Inside after setMatchObj: ${match.matchId}, $matchObj');
+  }
+
+  setMatchRules(String matchId, Rules rules) {
+    matchId = matchId;
+    rulesObj[matchId] = rules;
+    notifyListeners();
+  }
 }
--- lib/Providers/rules-provider.dart
+++ lib/Providers/rules-provider.dart
@@ -1,5 +1,13 @@
+import 'package:cric_scorer/Models/match-model.dart';
 import 'package:flutter/material.dart';
 
 class RulesProvider extends ChangeNotifier {
+  Map<String, Rules> rulesObj = new Map();
+  String matchIdForRules;
 
+  setMatchRules(String matchId, Rules rules) {
+    matchIdForRules = matchId;
+    rulesObj[matchId] = rules;
+    notifyListeners();
+  }
 }
