b2416d544a3d572577b100ca0f6743b41944e5ba
Kushal <kotikushal05@gmail.com>
2021-03-28 14:43:20 +0530

    Added constructors for the models

--- lib/Models/match-model.dart
+++ lib/Models/match-model.dart
@@ -1,16 +1,27 @@
 import 'user-model.dart';
 
+enum MatchFormat {
+  SINGLE_INNINGS, DOUBLE_INNINGS
+}
+
 class Rules {
   int numberOfConsecutiveWidesForARun;
   int numberOfNoBallsAllowed;
   int numberOfOversPerInnings;
-  int matchFormat; // Single Innings, Test
+  MatchFormat matchFormat; // Single Innings, Test
+
+  Rules() {
+    this.numberOfConsecutiveWidesForARun = 0;
+    this.numberOfOversPerInnings = 0;
+    this.numberOfNoBallsAllowed = 0;
+    this.matchFormat = MatchFormat.SINGLE_INNINGS;
+  }
 }
 
 
 class Match {
   String matchId;
-  DateTime matchDate;
+  String matchDate;
   String matchPlace;
   String matchStatus; // In Progress, Done, Abandoned
   String matchWinner;
@@ -23,7 +34,7 @@ class Match {
   List<User> teamMembers;
   List<User> opponentMembers;
   User middleMan;
-  String scorerEmail;
+  User scorer;
 
   List<User> currentBatsmen;
   User currentBowler;
@@ -34,4 +45,33 @@ class Match {
   List<int> opponentWickets;
 
   Rules matchRules;
+
+  Match() {
+    this.matchId = '';
+    this.matchId = '';
+    this.matchDate = '';
+    this.matchPlace = '';
+    this.matchStatus = ''; // In Progress, Done, Abandoned
+    this.matchWinner = '';
+
+    this.teamName = '';
+    this.teamSize = 0;
+    this.opponentName = '';
+    this.opponentSize = 0;
+
+    this.teamMembers = [];
+    this.opponentMembers = [];
+    this.middleMan = new User();
+    this.scorer = new User();
+
+    this.currentBatsmen = [];
+    this.currentBowler = new User();
+
+    this.teamScores = []; // First innings, Second Innings
+    this.teamWickets = [];
+    this.opponentScores = [];
+    this.opponentWickets = [];
+
+    this.matchRules = new Rules();
+  }
 }
--- lib/Models/user-model.dart
+++ lib/Models/user-model.dart
@@ -1,10 +1,19 @@
 class MatchRecords {
   List<User> bowled;
-  User bowledBy;
+  String bowledBy;
   List<User> caught;
-  User caughtBy;
+  String caughtBy;
   List<User> stumped;
-  User stumpedBy;
+  String stumpedBy;
+
+  MatchRecords() {
+    this.bowled = [];
+    this.bowledBy = '';
+    this.caught = [];
+    this.caughtBy = '';
+    this.stumped = [];
+    this.stumpedBy = '';
+  }
 }
 
 class Record {
@@ -28,14 +37,47 @@ class Record {
   double economy;
 
   MatchRecords extraMatchRecords;
+
+  Record() {
+    this.matches = 0;
+    this.matchesWon = 0;
+
+    this.ballsFaced = 0;
+    this.runsMade = 0;
+    this.fours = 0;
+    this.sixes = 0;
+
+    this.catches = 0;
+    this.stumps = 0;
+    this.runOuts = 0;
+
+    this.wickets = 0;
+    this.ballsBowled = 0;
+    this.overs = 0;
+    this.maidens = 0;
+    this.runsGiven = 0;
+    this.economy = 0;
+
+    this.extraMatchRecords = new MatchRecords();
+  }
 }
 
 class User {
-  String id;
+  String userId;
   String email;
   String phone;
   String name;
   Record record;
   List<Record> matchRecords;
   bool inAMatch;
+
+  User() {
+    this.userId = '';
+    this.email = '';
+    this.phone = '';
+    this.name = '';
+    this.record = new Record();
+    this.matchRecords = [];
+    this.inAMatch = false;
+  }
 }
