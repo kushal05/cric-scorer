ddb0441d23cae53d5ebdffcf7e518e26fd6fca59
Kushal <kotikushal05@gmail.com>
2021-03-28 14:47:28 +0530

    Taking user inputs for rules and updating them in providers

--- lib/Screens/rules-customisation-screen.dart
+++ lib/Screens/rules-customisation-screen.dart
@@ -1,4 +1,6 @@
+import 'package:cric_scorer/Models/match-model.dart';
 import 'package:cric_scorer/Providers/home-provider.dart';
+import 'package:cric_scorer/Providers/match-details-provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,6 +11,13 @@ class RulesCustomisation extends StatefulWidget {
 
 class _RulesCustomisationState extends State<RulesCustomisation> {
   int selectedFormat;
+  Match matchObject;
+
+  TextEditingController oversController = new TextEditingController();
+  TextEditingController widesController = new TextEditingController();
+  TextEditingController noBallsController = new TextEditingController();
+  MatchFormat formatController;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,48 +28,65 @@ class _RulesCustomisationState extends State<RulesCustomisation> {
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: TextField(
+                controller: oversController,
                 decoration: InputDecoration(
-                  labelText: 'Number Of Overs',
-                  hintText: 'Eg: 5, 10, 15, 20'
+                  labelText: 'Number Of Overs Per Innings',
+                  hintText: 'Eg: 5, 10, 15, 20',
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: TextField(
+                controller: widesController,
                 decoration: InputDecoration(
-                  labelText: 'Number Of Wides for a run',
+                  labelText: 'Number Of Consecutive Wides For A Run',
                   hintText: 'Eg: 1 (default), 2 (consecutive)',
                   hintStyle: TextStyle(fontSize: 15)
                 ),
               ),
             ),
-            DropdownButton(
-              hint: Text('Choose a Format'),
-              value: selectedFormat,
-              items: [
-                DropdownMenuItem(
-                  value: 1,
-                    child: Text('Single Innings')
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(
+                controller: noBallsController,
+                decoration: InputDecoration(
+                  labelText: 'Number Of No-Balls Allowed In An Over',
+                  hintText: 'Eg: 1 (default), 2 (extra)',
+                  hintStyle: TextStyle(fontSize: 15)
                 ),
-                DropdownMenuItem(
-                  value: 2,
-                    child: Text('Double Innings (Test)')
-                )
-              ],
-              onChanged: (val){
-                setState(() {
-                  selectedFormat = val;
-                });
-                print('Selected value: $val');
-              },
-              onTap: () {
-                  print('Tapped');
-              },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButton(
+                hint: Text('Choose a Format'),
+                value: selectedFormat,
+                items: [
+                  DropdownMenuItem(
+                    value: 1,
+                      child: Text('Single Innings')
+                  ),
+                  DropdownMenuItem(
+                    value: 2,
+                      child: Text('Double Innings (Test)')
+                  )
+                ],
+                onChanged: (val){
+                  setState(() {
+                    selectedFormat = val;
+                    (val == 1) ? formatController = MatchFormat.SINGLE_INNINGS : formatController = MatchFormat.DOUBLE_INNINGS;
+                  });
+                  print('Selected value: $val');
+                },
+              ),
             ),
             OutlineButton(
                 onPressed: () {
-                Provider.of<HomeProvider>(context, listen: false).updatePageDetails('scoring');
+                  matchObject = updateMatchObject();
+                  Provider.of<MatchDetailsProvider>(context, listen: false).setMatchObj(matchObject);
+                  Provider.of<MatchDetailsProvider>(context, listen: false).setMatchRules(matchObject.matchId, matchObject.matchRules);
+                  Provider.of<HomeProvider>(context, listen: false).updatePageDetails('scoring');
               },
               child: Text('Start the match'),
             )
@@ -68,5 +94,15 @@ class _RulesCustomisationState extends State<RulesCustomisation> {
         ),
       ),
     );
+  }
+
+  Match updateMatchObject() {
+    Match updateObj = new Match();
+    updateObj.matchId = "12340";
+    updateObj.matchRules.matchFormat = formatController;
+    updateObj.matchRules.numberOfConsecutiveWidesForARun = int.tryParse(widesController.text);
+    updateObj.matchRules.numberOfNoBallsAllowed = int.tryParse(noBallsController.text);
+    updateObj.matchRules.numberOfOversPerInnings = int.tryParse(oversController.text);
+    return updateObj;
   }
 }
