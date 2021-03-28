fd011501ab33f273b482c41d775f6fcf9becee69
Kushal <kotikushal05@gmail.com>
2021-03-28 13:11:19 +0530

    Added add players pop-up with dummy data

--- lib/Screens/team-selection-screen.dart
+++ lib/Screens/team-selection-screen.dart
@@ -18,13 +18,65 @@ class _TeamSelectionState extends State<TeamSelection> {
             Text('Team Selection Screen'),
             OutlineButton(
                 onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => openAddPlayersDialog(context)
+              );
+            },
+              child: Text('Add Players'),
+            ),
+            OutlineButton(
+                onPressed: () {
                   Provider.of<HomeProvider>(context, listen: false).updatePageDetails('rulesCustomisation');
                 },
-              child: Text('Customize Rules'),
+              child: Text('Add Rules'),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget openAddPlayersDialog(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return AlertDialog(
+      scrollable: true,
+      title: Text('Testing add players'),
+      content: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              TextField(),
+              Container(
+                height: height/2,
+                width: double.maxFinite,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 15,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text('Dummy User'),
+                      trailing: OutlineButton(
+                          onPressed: (){
+                            print('Added user to the team $index');
+                          },
+                        child: Text('Add to team'),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      actions: <Widget>[
+        OutlineButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          child: Text('Done'),
+        )
+      ],
     );
   }
 }
