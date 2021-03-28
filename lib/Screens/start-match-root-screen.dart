58a0611ddc73170ca346c79965394f46c9edb422
Kushal <kotikushal05@gmail.com>
2021-03-28 13:12:17 +0530

    Added loading widget till provider gets user availability details

--- lib/Screens/start-match-root-screen.dart
+++ lib/Screens/start-match-root-screen.dart
@@ -1,25 +1,48 @@
+import 'dart:async';
+
 import 'package:cric_scorer/Providers/home-provider.dart';
+import 'package:cric_scorer/Providers/start-match-provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class StartMatchRoot extends StatelessWidget {
+class StartMatchRoot extends StatefulWidget {
+  @override
+  _StartMatchRootState createState() => _StartMatchRootState();
+}
+
+class _StartMatchRootState extends State<StartMatchRoot> {
+
+  bool showLoader = true;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => updatePageBasedOnUserState());
+  }
+
+  void updatePageDelayed(){
+    Timer(Duration(seconds: 2), () => updatePageBasedOnUserState());
+  }
+
+  void updatePageBasedOnUserState() {
+    if(Provider.of<StartMatchProvider>(context, listen: false).canUserStartMatch(context)) {
+      Provider.of<HomeProvider>(context, listen: false).updatePageDetails('teamSelection');
+    } else {
+      Provider.of<HomeProvider>(context, listen: false).updatePageDetails('scoreView');
+    }
+    showLoader = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Start Match Root Screen'),
-              OutlineButton(
-                onPressed: () {
-                  Provider.of<HomeProvider>(context, listen: false).updatePageDetails('teamSelection');
-                },
-                child: Text('Team Selection'),
-              )
-            ],
-          )
-      ),
+    return Consumer<StartMatchProvider>(
+      builder: (context, data, _) {
+        return Container(
+          child: Center(
+              child: (showLoader) ? CircularProgressIndicator() : null
+          ),
+        );
+      },
     );
   }
 }
