dbeedf02edb798d16013b0864fa5bcf1cb31e7db
Kushal <kotikushal05@gmail.com>
2021-03-28 13:10:27 +0530

    Added some fields in rules-customisation

--- lib/Screens/rules-customisation-screen.dart
+++ lib/Screens/rules-customisation-screen.dart
@@ -8,6 +8,7 @@ class RulesCustomisation extends StatefulWidget {
 }
 
 class _RulesCustomisationState extends State<RulesCustomisation> {
+  int selectedFormat;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,12 +16,53 @@ class _RulesCustomisationState extends State<RulesCustomisation> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Rules Customisation'),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Number Of Overs',
+                  hintText: 'Eg: 5, 10, 15, 20'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Number Of Wides for a run',
+                  hintText: 'Eg: 1 (default), 2 (consecutive)',
+                  hintStyle: TextStyle(fontSize: 15)
+                ),
+              ),
+            ),
+            DropdownButton(
+              hint: Text('Choose a Format'),
+              value: selectedFormat,
+              items: [
+                DropdownMenuItem(
+                  value: 1,
+                    child: Text('Single Innings')
+                ),
+                DropdownMenuItem(
+                  value: 2,
+                    child: Text('Double Innings (Test)')
+                )
+              ],
+              onChanged: (val){
+                setState(() {
+                  selectedFormat = val;
+                });
+                print('Selected value: $val');
+              },
+              onTap: () {
+                  print('Tapped');
+              },
+            ),
             OutlineButton(
                 onPressed: () {
                 Provider.of<HomeProvider>(context, listen: false).updatePageDetails('scoring');
               },
-              child: Text('Score the match'),
+              child: Text('Start the match'),
             )
           ],
         ),
