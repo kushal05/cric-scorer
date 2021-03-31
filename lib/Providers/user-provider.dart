1cfbf55dca15d63c7112f6628d1aa892f07c18ff
Kushal <kotikushal05@gmail.com>
2021-04-01 00:23:06 +0530

    Adding dummy functions in user providers

--- lib/Providers/user-provider.dart
+++ lib/Providers/user-provider.dart
@@ -1,9 +1,25 @@
+import 'package:cric_scorer/Models/user-model.dart';
+import 'package:cric_scorer/Services/user-service.dart';
 import 'package:flutter/material.dart';
 
 class UserProvider extends ChangeNotifier {
 
+  List<User> usersList = [];
+  List<String> usersListString= ['hey', 'you'];
+
   bool isUserInAMatch() {
     // process this based on user details
     return false;
+  }
+
+  updateUserListString(List<String> list) {
+    usersListString = list;
+    notifyListeners();
+  }
+
+  getUsersFromSearch() {
+    String email = 'test_user@gmail.com';
+    UserService userService = new UserService();
+    userService.searchForUsers(email);
   }
 }
