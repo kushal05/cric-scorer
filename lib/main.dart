a1a67d1afd54863dda61cda2f772179eaebf2087
Kushal <kotikushal05@gmail.com>
2021-04-01 00:25:06 +0530

    Adding support to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:cric_scorer/Screens/root-screen.dart';
 import 'package:cric_scorer/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
